57a8f5719e5407200fddbfc7c584ab0f0b78ad93
Karan <karanchandekar431@gmail.com>
2023-01-23 10:18:54 +0530

    created and styled hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import MyProfileImg from "../images/karan.jpg";
+import Link from "next/link";
 
 type Props = {};
 
@@ -32,10 +35,35 @@ const Hero = (props: Props) => {
       className="flex flex-col items-center justify-center h-screen space-y-8 overflow-hidden text-center"
     >
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#f7ab0a" />
-      </h1>
+      <Image
+        className="relative object-cover w-32 h-32 mx-auto rounded-full"
+        src={MyProfileImg}
+        alt="Karan's Profile Picture"
+      />
+      <div className="z-20">
+        <h2 className="pb-2 text-sm text-gray-500 uppercase tracking-[15px]">
+          Frontend Developer
+        </h2>
+        <h1 className="px-10 text-5xl font-semibold lg:text-6xl">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#f7ab0a" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </motion.div>
   );
 };
