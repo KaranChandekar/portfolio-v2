2ee8e4d6860503363011c9a454a8130ab528ef65
Karan <karanchandekar431@gmail.com>
2023-01-24 10:06:45 +0530

    update

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -19,11 +19,11 @@ const BackgroundCircles = (props: Props) => {
       }}
       className="relative flex items-center justify-center"
     >
-      <div className="absolute border border-[#333333] rounded-full md:h-[150px] md:w-[150px] h-[100px] w-[100px] mt-52 animate-ping" />
-      <div className="absolute border border-[#333333] rounded-full md:h-[250px] md:w-[250px] h-[150px] w-[150px] mt-52" />
-      <div className="absolute border border-[#333333] rounded-full md:h-[450px] md:w-[450px] h-[350px] w-[350px] mt-52" />
-      <div className="absolute border rounded-full border-[#f7ab0a] opacity-20 md:h-[600px] md:w-[600px] h-[500px] w-[500px] mt-52 animate-pulse" />
-      <div className="border rounded-full border-[#333333] md:h-[750px] md:w-[750px] h-[650px] w-[650px] mt-52 absolute" />
+      <div className="absolute border border-[#333333] rounded-full md:h-[200px] md:w-[200px] h-[150px] w-[150px] mt-[185px] animate-ping" />
+      <div className="absolute border border-[#333333] rounded-full md:h-[250px] md:w-[250px] h-[200px] w-[200px] mt-[185px]" />
+      <div className="absolute border border-[#333333] rounded-full md:h-[450px] md:w-[450px] h-[300px] w-[300px] mt-[185px]" />
+      <div className="absolute border rounded-full border-[#f7ab0a] opacity-20 md:h-[600px] md:w-[600px] h-[400px] w-[400px] mt-[185px] animate-pulse" />
+      <div className="border rounded-full border-[#333333] md:h-[750px] md:w-[750px] h-[500px] w-[500px] mt-[185px] absolute" />
     </motion.div>
   );
 };
