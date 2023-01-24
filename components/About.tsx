615700f73ce6f1ce5ef4c10b1921dcb85e791850
Karan <karanchandekar431@gmail.com>
2023-01-24 10:50:08 +0530

    update

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ const About = (props: Props) => {
       transition={{ duration: 1.5 }}
       className="relative flex flex-col items-center h-screen px-4 mx-auto text-center md:px-10 md:text-left md:flex-row max-w-7xl justify-evenly"
     >
-      <h3 className="absolute uppercase top-24 tracking-[24px] text-gray-500 text-2xl">
+      <h3 className="absolute uppercase top-16 tracking-[24px] text-gray-500 text-2xl">
         About
       </h3>
 
