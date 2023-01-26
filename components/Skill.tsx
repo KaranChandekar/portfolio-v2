9ef5ceeee7abbdb8520da8d3779298fe1b54a5c1
Karan <karanchandekar431@gmail.com>
2023-01-26 10:13:54 +0530

    added skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,32 +1,28 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-type Props = {
-  directionLeft?: boolean;
-};
-
-const Skill = (directionLeft: Props) => {
+function Skill(props: any) {
   return (
     <div className="relative flex cursor-pointer group">
       <motion.img
         initial={{
-          x: directionLeft ? -100 : 100,
+          x: -100,
           opacity: 0,
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
-        src="https://skillicons.dev/icons?i=react"
+        src={props.link}
         className="object-cover w-12 h-12 transition duration-300 ease-in-out border border-gray-500 rounded-full md:w-20 md:h-20 filter group-hover:grayscale"
       />
       <div className="absolute z-0 w-12 h-12 transition duration-300 ease-in-out rounded-full opacity-0 group-hover:bg-white group-hover:opacity-80 md:w-20 md:h-20">
         <div className="flex items-center justify-center h-full">
           <p className="text-xs font-bold text-black opacity-100 xl:text-base">
-            100%
+            {props.level}
           </p>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Skill;
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -5,6 +5,25 @@ import Skill from "./Skill";
 type Props = {};
 
 const Skills = (props: Props) => {
+  const skills = [
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-line.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-line.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+  ];
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -21,20 +40,22 @@ const Skills = (props: Props) => {
       </h3>
 
       <div className="grid grid-cols-4 gap-5">
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        <Skill link={skills[0]} level="90%" />
+        <Skill link={skills[1]} level="90%" />
+        <Skill link={skills[2]} level="80%" />
+        <Skill link={skills[3]} level="60%" />
+        <Skill link={skills[4]} level="70%" />
+        <Skill link={skills[5]} level="70%" />
+        <Skill link={skills[6]} level="90%" />
+        <Skill link={skills[7]} level="90%" />
+        <Skill link={skills[8]} level="60%" />
+        <Skill link={skills[9]} level="60%" />
+        <Skill link={skills[10]} level="60%" />
+        <Skill link={skills[11]} level="70%" />
+        <Skill link={skills[12]} level="90%" />
+        <Skill link={skills[13]} level="90%" />
+        <Skill link={skills[14]} level="90%" />
+        <Skill link={skills[15]} level="90%" />
       </div>
     </motion.div>
   );
