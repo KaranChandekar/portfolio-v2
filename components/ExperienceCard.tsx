00a75998853a31782c18cda75b9234a569b70228
Karan <karanchandekar431@gmail.com>
2023-01-26 10:56:57 +0530

    update experience card

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
-type Props = {};
-
-const ExperienceCard = (props: Props) => {
+function ExperienceCard(props: any) {
   return (
     <article className="flex flex-col flex-shrink-0 rounded-lg space-y-5 w-full sm:w-[500px] snap-center bg-[#292929] p-4 md:p-10 md:opacity-60 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-y-scroll scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 scrollbar-thin">
       <motion.img
@@ -22,48 +21,91 @@ const ExperienceCard = (props: Props) => {
           once: true,
         }}
         className="object-cover object-center w-32 h-32 rounded-full"
-        src="https://i.postimg.cc/qRxKFr4D/ucpi.jpg"
+        src={props.logo}
         alt="YoungNomads"
       />
 
       <div className="px-0">
-        <h4 className="text-2xl font-light md:text-3xl">Frontend Developer</h4>
-        <p className="mt-1 text-xl font-bold">UCPI</p>
+        <h4 className="text-2xl font-light md:text-3xl">{props.title}</h4>
+        <p className="mt-1 text-xl font-bold">{props.company}</p>
         <div className="flex my-2 space-x-2">
-          <img
-            className="w-8 rounded-full"
-            src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png?w=128&f=avif"
+          <Image
+            width={30}
+            height={30}
+            className="rounded-full"
+            src={props.skills[0]}
             alt="js"
           />
-          <img
-            className="w-8 rounded-full"
-            src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png?w=128&f=avif"
+          <Image
+            width={30}
+            height={30}
+            className="rounded-full"
+            src={props.skills[1]}
             alt="js"
           />
-          <img
-            className="w-8 rounded-full"
-            src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png?w=128&f=avif"
+          <Image
+            width={30}
+            height={30}
+            className="rounded-full"
+            src={props.skills[2]}
             alt="js"
           />
-          <img
-            className="w-8 rounded-full"
-            src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png?w=128&f=avif"
+          <Image
+            width={30}
+            height={30}
+            className="rounded-full"
+            src={props.skills[3]}
+            alt="js"
+          />
+          <Image
+            width={30}
+            height={30}
+            className="rounded-full"
+            src={props.skills[4]}
+            alt="js"
+          />
+          <Image
+            width={30}
+            height={30}
+            className="rounded-full"
+            src={props.skills[5]}
+            alt="js"
+          />
+          <Image
+            width={30}
+            height={30}
+            className="rounded-full"
+            src={props.skills[6]}
+            alt="js"
+          />
+          <Image
+            width={30}
+            height={30}
+            className="rounded-full"
+            src={props.skills[7]}
+            alt="js"
+          />
+          <Image
+            width={30}
+            height={30}
+            className="rounded-full"
+            src={props.skills[8]}
             alt="js"
           />
         </div>
         <p className="py-3 text-gray-300 uppercase">
-          Started work.... - Ended...
+          {props.startDate} - {props.endDate}
         </p>
         <ul className="ml-5 space-y-2 text-sm list-disc lg:text-base">
-          <li>Summary Points Summary Points Summary Points Summary Points</li>
-          <li>Summary Points Summary Points Summary Points Summary Points</li>
-          <li>Summary Points Summary Points Summary Points Summary Points</li>
-          <li>Summary Points Summary Points Summary Points Summary Points</li>
-          <li>Summary Points Summary Points Summary Points Summary Points</li>
+          <li>{props.summary[0]}</li>
+          <li>{props.summary[1]}</li>
+          <li>{props.summary[2]}</li>
+          <li>{props.summary[3]}</li>
+          <li>{props.summary[4]}</li>
         </ul>
       </div>
     </article>
   );
-};
+}
 
 export default ExperienceCard;
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -5,6 +5,89 @@ import ExperienceCard from "./ExperienceCard";
 type Props = {};
 
 const WorkExperience = (props: Props) => {
+  const jobs = [
+    {
+      id: "3",
+      title: "Frontend Developer",
+      company: "YOUNG NOMADS",
+      logo: "https://i.postimg.cc/CMNd1bX1/youngnomads.jpg",
+      skills: [
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+      ],
+      startDate: "January 2023",
+      endDate: "Running",
+      summary: [
+        "Young Nomads is a Digital Marketing company led by Ayush Gupta.",
+        "Work & Responsibilities - To design and create websites for company products.",
+        "Build websites with latest web technologies.",
+        "Designed and created official website for company.",
+        "Handled the frontend side for various products in company.",
+      ],
+    },
+
+    {
+      id: "2",
+      title: "Frontend Developer",
+      company: "UCPI",
+      logo: "https://i.postimg.cc/qRxKFr4D/ucpi.jpg",
+      skills: [
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+      ],
+      startDate: "October 2022",
+      endDate: "January 2023",
+      summary: [
+        "UCPI is a blockchain based Web3 start-up company.",
+        "Work & Responsibilities - To design and create websites for company products in React.",
+        "Designed and created official website for company.",
+        "Designed and created website for product DAuth.",
+        "Designed and created UI for product ucpimapping.",
+        "Got experience about how to work in a team and collaborate with other developers.",
+      ],
+    },
+    {
+      id: 1,
+      title: "Web Devloper",
+      company: "100DAYSOFCODE",
+      logo: "https://i.postimg.cc/N06BncwM/100daysofcode.jpg",
+      skills: [
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+      ],
+      startDate: "December 2021",
+      endDate: "April 2022",
+      summary: [
+        "Did coding daily for 100 days straight.",
+        "Learnt the building blocks of the web like HTML, CSS and JavaScript.",
+        "Created more than 50 projects and pushed it on GitHub.",
+        "It made me a very consistent coder.",
+        "Started coding from 0 and now at 500 + days of consistency.",
+      ],
+    },
+  ];
+
   return (
     <motion.div
       initial={{
@@ -21,9 +104,33 @@ const WorkExperience = (props: Props) => {
       </h3>
 
       <div className="flex w-full md:px-10 py-32 space-x-10 overflow-x-scroll snap-x snap-mandatory scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 scrollbar-thin h-screen">
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
+        <ExperienceCard
+          title={jobs[0].title}
+          company={jobs[0].company}
+          logo={jobs[0].logo}
+          skills={jobs[0].skills}
+          startDate={jobs[0].startDate}
+          endDate={jobs[0].endDate}
+          summary={jobs[0].summary}
+        />
+        <ExperienceCard
+          title={jobs[1].title}
+          company={jobs[1].company}
+          logo={jobs[1].logo}
+          skills={jobs[1].skills}
+          startDate={jobs[1].startDate}
+          endDate={jobs[1].endDate}
+          summary={jobs[1].summary}
+        />
+        <ExperienceCard
+          title={jobs[2].title}
+          company={jobs[2].company}
+          logo={jobs[2].logo}
+          skills={jobs[2].skills}
+          startDate={jobs[2].startDate}
+          endDate={jobs[2].endDate}
+          summary={jobs[2].summary}
+        />
       </div>
     </motion.div>
   );
