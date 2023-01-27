5152a9c36633cb747d00f985d27809cdb0f568a1
Karan <karanchandekar431@gmail.com>
2023-01-27 21:07:05 +0530

    update projects section

--- components/Project.tsx
+++ components/Project.tsx
@@ -6,24 +6,27 @@ import Link from "next/link";
 const Project = (props: any) => {
   return (
     <div className="flex flex-col items-center justify-center flex-shrink-0 w-screen h-screen p-4 space-y-5 snap-center md:p-10">
-      <motion.img
-        initial={{
-          y: -300,
-          opacity: 0,
-        }}
-        whileInView={{
-          y: 0,
-          opacity: 1,
-        }}
-        transition={{
-          duration: 1.2,
-        }}
-        viewport={{
-          once: true,
-        }}
-        src={props.image}
-        alt=""
-      />
+      <div className="max-w-2xl px-0 space-y-5 md:px-10">
+        <motion.img
+          initial={{
+            y: -300,
+            opacity: 0,
+          }}
+          whileInView={{
+            y: 0,
+            opacity: 1,
+          }}
+          transition={{
+            duration: 1.2,
+          }}
+          viewport={{
+            once: true,
+          }}
+          src={props.image}
+          alt=""
+          className="w-full"
+        />
+      </div>
 
       <div className="max-w-6xl px-0 space-y-5 md:px-10">
         <h4 className="text-2xl font-semibold text-center md:text-4xl">
