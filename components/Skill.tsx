5160f05d823ba3b03cbf6357c1cb2efc1608fd55
Karan <karanchandekar431@gmail.com>
2023-01-30 20:31:12 +0530

    update skills section

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -12,9 +12,9 @@ function Skill(props: any) {
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
         src={props.link}
-        className="object-cover w-12 h-12 transition duration-300 ease-in-out border border-gray-500 rounded-full md:w-20 md:h-20 filter group-hover:grayscale"
+        className="object-cover w-12 h-12 transition duration-300 ease-in-out border border-gray-500 rounded-full md:w-16 md:h-16 filter group-hover:grayscale"
       />
-      <div className="absolute z-0 w-12 h-12 transition duration-300 ease-in-out rounded-full opacity-0 group-hover:bg-white group-hover:opacity-80 md:w-20 md:h-20">
+      <div className="absolute z-0 w-12 h-12 transition duration-300 ease-in-out rounded-full opacity-0 group-hover:bg-white group-hover:opacity-80 md:w-16 md:h-16">
         <div className="flex items-center justify-center h-full">
           <p className="text-xs font-bold text-black opacity-100 xl:text-base">
             {props.level}
